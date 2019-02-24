15b4925ff5b389fb6882381ad47deded6b6532d7
mgusmano <mgusmano@yahoo.com>
2019-02-24 01:46:44 -0500

    clean up

--- packages/ext-components-kitchensink/src/view/main/main.js
+++ packages/ext-components-kitchensink/src/view/main/main.js
@@ -1,46 +1,30 @@
+import hljs from 'highlight.js/lib/highlight'
+hljs.registerLanguage('typescript', require('highlight.js/lib/languages/typescript'));
+hljs.registerLanguage('http', require('highlight.js/lib/languages/http'));
+hljs.registerLanguage('css', require('highlight.js/lib/languages/css'));
+hljs.registerLanguage('xml', require('highlight.js/lib/languages/xml'));
+hljs.registerLanguage('javascript', require('highlight.js/lib/languages/javascript'));
+
 export default class main {
 
-  constructor() {
+  constructor() {}
+
+  toggleCode() {
+    var collapsed = this.codePanelCmp.getHidden()
+    if(collapsed == true) { collapsed = false }
+    else { collapsed = true }
+    this.codePanelCmp.setHidden(collapsed)
   }
 
-  transform(node, parentUrl) {
-    node.leaf = !node.hasOwnProperty('children');
-    node.iconCls = node.navIcon;
-    if (node.text && !node.id) {
-        node.id = (parentUrl === '/' ? '' : parentUrl) + '/' + node.text.toLowerCase().replace(/\s/g, '_').replace(/[^\w]/g, '');
-    }
-    node.name = node.text;
-    if (node.children) {
-        node.children = node.children.filter(node => !node.hidden);
-        node.children.forEach(child => this.transform(child, node.id))
-    }
+  toggleTree() {
+    var collapsed = this.treePanelCmp.getCollapsed()
+    if (collapsed == true){collapsed = false} else{collapsed = true}
+    this.treePanelCmp.setCollapsed(collapsed)
   }
 
-  // getFiles = (node, allCode) => {
-  //   const name = node.data.name.replace(/\s+/g, "");
-  //   const codeMap = allCode[name] || {};
-  //   const files = Object.keys(codeMap).map((file) => {
-  //     return {
-  //       name: file,
-  //       code: `<pre><code mwlHighlightJs id='${file}' class='code ${cssClassName(file)}'>${codeMap[file].replace(/</g, '&lt;').replace(/>/g, '&gt;')}</code></pre>`,
-  //       //code: `<pre><code mwlHighlightJs [source]="'${codeMap[file].replace(/</g, '&lt;').replace(/>/g, '&gt;')}'" language="typescript"></code></pre>`
-  //     };
-  //   });
-  
-  //   return files;
-  // };
-
-
-  selectionchange(event) {
-    var record = event.detail.record
-    var hash = record.data.hash
-    var childNum = record.childNodes.length
-    if (childNum == 0 && hash != undefined) {
-      window.location.hash = '#' + hash
-      this.setCodeTabs()
-      var className = record.data.className
-      window[className.name] = new className()
-    }
+  readyTreePanel(event) {
+    var cmp = event.detail.cmp
+    this.treePanelCmp = cmp
   }
 
   readyTreelist(event) {
@@ -60,6 +44,31 @@ export default class main {
     treelistCmp.setSelection(node);
   }
 
+  transform(node, parentUrl) {
+    node.leaf = !node.hasOwnProperty('children');
+    node.iconCls = node.navIcon;
+    if (node.text && !node.id) {
+        node.id = (parentUrl === '/' ? '' : parentUrl) + '/' + node.text.toLowerCase().replace(/\s/g, '_').replace(/[^\w]/g, '');
+    }
+    node.name = node.text;
+    if (node.children) {
+        node.children = node.children.filter(node => !node.hidden);
+        node.children.forEach(child => this.transform(child, node.id))
+    }
+  }
+
+  selectionchange(event) {
+    var record = event.detail.record
+    var hash = record.data.hash
+    var childNum = record.childNodes.length
+    if (childNum == 0 && hash != undefined) {
+      window.location.hash = '#' + hash
+      this.setCodeTabs()
+      var className = record.data.className
+      window[className.name] = new className()
+    }
+  }
+
   setCodeTabs() {
     var codeMap = _code[window.location.hash.substr(1)]
     var me = this
@@ -67,41 +76,19 @@ export default class main {
       me.tabPanelCmp.removeAll()
       Object.keys(codeMap).map((file) => {
         me.tabPanelCmp.add({
-          xtype:'container',
-          tab:{ui: 'app-code-tab', flex: 0, minWidth: 120},
-          title:file,
-          html:`<pre><code mwlHighlightJs id='${file}' class='code '>${codeMap[file].replace(/</g, '&lt;').replace(/>/g, '&gt;')}</code></pre>`
+          xtype: 'panel',
+          ui: 'code-panel',
+          tab: {ui: 'app-code-tab', flex: 0, minWidth: 120},
+          title: file,
+          html: `<pre><code mwlHighlightJs id='${file}' class='code '>${codeMap[file].replace(/</g, '&lt;').replace(/>/g, '&gt;')}</code></pre>`
         })
       })
+      document.querySelectorAll(".code").forEach((el) => {
+        console.log('here')
+        console.log(el)
+        hljs.highlightBlock(el);
+       });
     }
-  }
-
-  test() {
-    console.log('in test')
-  }
-
-  readyTree(event) {
-    var cmp = event.detail.cmp
-    this.treeCmp = cmp
-  }
-
-  toggleTree() {
-    var collapsed = this.treeCmp.getCollapsed()
-    if (collapsed == true){collapsed = false} else{collapsed = true}
-    this.treeCmp.setCollapsed(collapsed)
-    console.log('in toggleTree')
-  }
-
-  toggleCode() {
-    console.log('in toggleCode')
-    var collapsed = this.codePanelCmp.getHidden()
-    if(collapsed == true) { collapsed = false }
-    else { collapsed = true }
-    this.codePanelCmp.setHidden(collapsed)
-  }
-
-  tap(event) {
-    console.log('in tap')
   }
 
   readyCodePanel(event) {
@@ -112,15 +99,6 @@ export default class main {
   readyTabPanel(event) {
     var cmp = event.detail.cmp
     this.tabPanelCmp = cmp
-  }
-
-  readyButton1(event) {
-    //var cmp = event.detail.cmp
-  }
-
-  readyButton2(event) {
-    var cmp = event.detail.cmp
-    cmp.setText('goodbye')
   }
 
 }
