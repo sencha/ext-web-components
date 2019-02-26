f4b94c1535a04c608bfb028968b71afd67c48653
mgusmano <mgusmano@yahoo.com>
2019-02-25 19:22:03 -0500

    main

--- packages/ext-components-kitchensink/src/app.js
+++ packages/ext-components-kitchensink/src/app.js
@@ -9,7 +9,7 @@ import MainComponent from './view/main/MainComponent.js';
   function init() {
     window.examplesRoot = './src/view/'
     window.main = new MainComponent()
-    document.body.innerHTML = window._code['main']['MainComponent' + '.html']
+    document.body.innerHTML = window._code['main']['MainComponent.html']
     window.menu = getMenu();
     window.routes = [];
     getRoutes(window.menu, window.location.hash.substr(1), window.examplesRoot);
@@ -21,10 +21,10 @@ import MainComponent from './view/main/MainComponent.js';
       item.path = path + item.hash + '/';
       if (item.children == undefined) {
         if (item.default == undefined) { item.default = false }
-        if(item.hash == hash) {
-          window[hash] = new item.component()
-          window.initHash = hash
-        }
+        // if(item.hash == hash) {
+        //   window[hash] = new item.component()
+        //   window.initHash = hash
+        // }
         window.routes.push(new Route(item.hash, item.component, item.path, item.default))
       }
       else {
