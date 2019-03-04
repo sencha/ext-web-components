e0a904b6a3acef1544fe1be7a2475bc825499265
mgusmano <mgusmano@yahoo.com>
2019-03-04 13:37:21 -0500

    routing

--- packages/ext-components-kitchensink/src/app.js
+++ packages/ext-components-kitchensink/src/app.js
@@ -7,15 +7,9 @@ import MainComponent from './view/main/MainComponent.js';
   init();
 
   function init() {
-    //window.examplesRoot = './src/view/'
     window.menu = getMenu();
     window.routes = [];
-    console.log('window.menu')
-    console.dir(window.menu)
-    //getRoutes(window.menu, window.location.hash.substr(1), window.examplesRoot);
     getRoutes(window.menu);
-    console.log('window.routes')
-    console.dir(window.routes)
     window.main = new MainComponent()
     document.body.innerHTML = window._code['main']['MainComponent.html']
   }
@@ -24,7 +18,6 @@ import MainComponent from './view/main/MainComponent.js';
     items.forEach(function(item){
       item.leaf = !item.hasOwnProperty('children');
       item.hash = item.text.toLowerCase().replace(/\s/g, '');
-      //item.id = item.hash;
       if (item.children == undefined) {
         window.routes.push(new Route(item.hash, item.component, item.default))
       }
--- packages/ext-components-kitchensink/src/view/about/AboutComponent.js
+++ packages/ext-components-kitchensink/src/view/about/AboutComponent.js
@@ -1,5 +1,9 @@
 export default class AboutComponent {
 
+  constructor() {
+    console.log('in about')
+  }
+
   readyButton1(event) {
     var cmp = event.detail.cmp
   }
