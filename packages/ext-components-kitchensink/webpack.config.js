cda21e3b150066849a600293cf98daa7f0ac3c4c
mgusmano <mgusmano@yahoo.com>
2019-03-12 22:05:47 -0400

    webpack config variables

--- packages/ext-components-kitchensink/webpack.config.js
+++ packages/ext-components-kitchensink/webpack.config.js
@@ -5,29 +5,21 @@ const ExtWebpackPlugin = require('@sencha/ext-webpack-plugin')
 const portfinder = require('portfinder')
 
 module.exports = function (env) {
-  var browserprofile
-  var watchprofile
-  var buildenvironment = env.environment || process.env.npm_package_extbuild_defaultenvironment
-  if (buildenvironment == 'production') {
-    browserprofile = false
-    watchprofile = 'no'
-  }
-  else {
-    if (env.browser == undefined) {env.browser = true}
-    browserprofile = JSON.parse(env.browser) || true
-    watchprofile = env.watch || 'yes'
-  }
-  const isProd = buildenvironment === 'production'
-  var buildprofile = env.profile || process.env.npm_package_extbuild_defaultprofile
-  var buildenvironment = env.environment || process.env.npm_package_extbuild_defaultenvironment
-  var buildverbose = env.verbose || process.env.npm_package_extbuild_defaultverbose
-  if (buildprofile == 'all') { buildprofile = '' }
-  if (env.treeshake == undefined) {env.treeshake = false}
-  var treeshake = env.treeshake ? JSON.parse(env.treeshake) : false
-  //var basehref = env.basehref || '/'
-  var mode = isProd ? 'production': 'development'
-  var outputFolder = 'build'
+  function get(it, val) {if(env == undefined) {return val} else if(env[it] == undefined) {return val} else {return env[it]}}
 
+  var profile     = get('profile',     '')
+  var environment = get('environment', 'development')
+  var treeshake   = get('treeshake',   'no')
+  var browser     = get('browser',     'yes')
+  var watch       = get('watch',       'yes')
+  var verbose     = get('verbose',     'no')
+  if (environment == 'production') {
+    browser = 'no'
+    watch = 'no'
+  }
+  const isProd = environment === 'production'
+  //const mode = isProd ? 'production': 'development'
+  const outputFolder = 'build'
   portfinder.basePort = (env && env.port) || 1962
   return portfinder.getPortPromise().then(port => {
     const plugins = [
@@ -36,15 +28,9 @@ module.exports = function (env) {
         framework: 'components',
         toolkit: 'modern',
         theme: 'theme-kitchensink',
-        profile: buildprofile, 
-        environment: buildenvironment,
-        treeshake: treeshake,
-        port: port,
-        emit: true,
-        browser: browserprofile,
-        watch: watchprofile,
-        verbose: buildverbose,
+        emit: 'yes',
         script: './extract-code.js',
+        port: port,
         packages: [
           'font-ext', 
           'ux', 
@@ -56,7 +42,13 @@ module.exports = function (env) {
           'calendar', 
           'charts',
           'treegrid'
-        ]
+        ],
+        profile: profile, 
+        environment: environment,
+        treeshake: treeshake,
+        browser: browser,
+        watch: watch,
+        verbose: verbose
       }),
       // new CopyWebpackPlugin([
       //   {from: 'copy/extjs',to: 'extjs'},
@@ -65,8 +57,8 @@ module.exports = function (env) {
       // ]),
     ]
     return {
-      mode: mode,
-      devtool: (mode === 'development') ? 'inline-source-map' : false,
+      mode: environment,
+      devtool: (environment === 'development') ? 'inline-source-map' : false,
       entry: './src/app.js',
       output: {
         path: path.join(__dirname, outputFolder),
@@ -77,20 +69,12 @@ module.exports = function (env) {
       module: {
         rules: [
           { test: /\.(js)$/, exclude: /node_modules/, use: ['babel-loader'] },
-          {
-            test: /\.(html)$/,
-            use: { loader: 'html-loader' }
-          },
-          {
-            test: /\.css$/,
-            use: ['style-loader','css-loader']
-          }
+          { test: /\.(html)$/,use: { loader: 'html-loader' } },
+          { test: /\.css$/,use: ['style-loader','css-loader'] }
         ]
       },
       stats: 'none',
-      optimization: {
-        noEmitOnErrors: true
-      },
+      optimization: { noEmitOnErrors: true },
       node: false,
       devServer: {
         contentBase: outputFolder,
@@ -104,5 +88,5 @@ module.exports = function (env) {
         stats: 'none'
       }
     }
-})
+  })
 }
