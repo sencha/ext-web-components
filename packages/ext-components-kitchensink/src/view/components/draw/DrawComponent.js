b517b4bdf8da107164475eb38ab69d66d52c97a1
Shivani Sharma <sharma.14shivani@gmail.com>
2019-03-15 19:01:12 +0530

    Removed commented code.

--- packages/ext-components-kitchensink/src/view/components/draw/DrawComponent.js
+++ packages/ext-components-kitchensink/src/view/components/draw/DrawComponent.js
@@ -5,15 +5,6 @@ export default class DrawComponent {
 
   constructor () {}
 
-  // readyButton1(event) {
-  //   var cmp = event.detail.cmp;
-  //   this.button1Cmp = event.detail.cmp;
-  // }
-
-  // tapButton1(event) {
-  //   this.button1Cmp.setText(new Date())
-  // }
-
   refs = {
     draw: {}
   };
