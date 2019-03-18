1408908d13066261ba77d494621b1ad675d7d6c9
Palak Rathi <palakrathi@CelSys053-Palak-Rathi.local>
2019-03-18 22:25:17 +0530

    Added all components of forms

--- packages/ext-components-kitchensink/src/view/components/forms/radiofield/RadioFieldComponent.js
+++ packages/ext-components-kitchensink/src/view/components/forms/radiofield/RadioFieldComponent.js
@@ -1,18 +1,9 @@
 import './RadioFieldComponent.css';
+import './RadioFieldComponent.html';
 
 export default class RadioFieldComponent {
 
   constructor () {
     console.log('in RadioFieldComponent constructor');
   }
-
-  readyButton1(event) {
-    var cmp = event.detail.cmp;
-    this.button1Cmp = event.detail.cmp;
-  }
-
-  tapButton1(event) {
-    this.button1Cmp.setText(new Date())
-  }
-
 }
--- packages/ext-components-kitchensink/src/view/components/forms/timefield/TimeFieldComponent.js
+++ packages/ext-components-kitchensink/src/view/components/forms/timefield/TimeFieldComponent.js
@@ -1,18 +1,36 @@
 import './TimeFieldComponent.css';
+import './TimeFieldComponent.html';
+Ext.require('Ext.field.InputMask'); // need to require this specifically for inputMask props to work
 
 export default class TimeFieldComponent {
-
   constructor () {
+
+    this.disabled = false;
+
     console.log('in TimeFieldComponent constructor');
   }
 
-  readyButton1(event) {
-    var cmp = event.detail.cmp;
-    this.button1Cmp = event.detail.cmp;
+  toggleDisable(event) {
+    this.disabled = !this.disabled;
+    let buttonText = "Disable All";
+
+    if (this.disabled) {
+      buttonText = "Enable All";
+    }
+
+    this.buttonCmp.setText(buttonText);
+    this.timefieldCmp.setDisabled(this.disabled);
   }
 
-  tapButton1(event) {
-    this.button1Cmp.setText(new Date())
+  timefieldready(event) {
+    this.timefieldCmp = event.detail.cmp;
   }
 
+  buttonready(event) {
+    this.buttonCmp = event.detail.cmp;
+  }
+
+  resettimefield() {
+    this.timefieldCmp.reset(true);
+  }
 }
