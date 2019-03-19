b36873e8d129336e1b1cab096092938353a6ccfa
mgusmano <mgusmano@yahoo.com>
2019-03-19 10:02:08 -0400

    ux resources

--- packages/ext-components-kitchensink/webpack.config.js
+++ packages/ext-components-kitchensink/webpack.config.js
@@ -1,7 +1,9 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { BaseHrefWebpackPlugin } = require('base-href-webpack-plugin');
-const ExtWebpackPlugin = require('@sencha/ext-webpack-plugin')
+const ExtWebpackPlugin = require('@sencha/ext-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
 const portfinder = require('portfinder')
 
 module.exports = function (env) {
@@ -48,7 +50,11 @@ module.exports = function (env) {
         browser: browser,
         watch: watch,
         verbose: verbose
-      })
+      }),
+      new CopyWebpackPlugin([{
+        from: '../node_modules/@sencha/ext-ux/modern/resources',
+        to: './ext/ux'
+      }])
     ]
     return {
       mode: environment,
