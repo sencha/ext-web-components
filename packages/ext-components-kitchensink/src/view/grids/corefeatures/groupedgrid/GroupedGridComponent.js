0eb0abd26506d766a1112003760e0890da210895
Rahulgarg30591 <rgarg@celestialsys.com>
2019-03-25 17:49:25 +0530

    Initial Commit of Grouped Grid.

--- packages/ext-components-kitchensink/src/view/grids/corefeatures/groupedgrid/GroupedGridComponent.js
+++ packages/ext-components-kitchensink/src/view/grids/corefeatures/groupedgrid/GroupedGridComponent.js
@@ -1,18 +1,29 @@
-import './GroupedGridComponent.css';
+import './RestaurantData';
+
+Ext.require([
+    'Ext.grid.cell.Number',
+    'Ext.grid.cell.Widget',
+    'Ext.grid.SummaryRow',
+    'Ext.ux.rating.Picker'
+]);
 
 export default class GroupedGridComponent {
-
   constructor () {
     console.log('in GroupedGridComponent constructor');
   }
 
-  readyButton1(event) {
-    var cmp = event.detail.cmp;
-    this.button1Cmp = event.detail.cmp;
-  }
+  onReady(event) {
+    const store = Ext.create('Ext.data.Store', {
+        autoLoad: true,
+        proxy: {
+            type: 'ajax',
+            url: '/KitchenSink/Restaurants'
+        },
+        sorters: ['cuisine', 'name'],
+        groupField: 'cuisine'
+    });
 
-  tapButton1(event) {
-    this.button1Cmp.setText(new Date())
+    this.grid = event.detail.cmp;
+    this.grid.setStore(store);
   }
-
 }
