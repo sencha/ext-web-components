a8329c3dd900bcef53cebe6e5396c62d25f0df2f
Rahulgarg30591 <rgarg@celestialsys.com>
2019-03-26 14:00:12 +0530

    EWC-4-Ks-Tabs Scrolling Tabs are half done, minWidth implementation is left.

--- packages/ext-components-kitchensink/src/view/components/tabs/scrollingtabs/ScrollingTabsComponent.js
+++ packages/ext-components-kitchensink/src/view/components/tabs/scrollingtabs/ScrollingTabsComponent.js
@@ -1,4 +1,5 @@
 Ext.require('Ext.layout.overflow.Scroller');
+import './ScrollingTabsComponent.html';
 
 export default class ScrollingTabsComponent {
   constructor () {
@@ -6,13 +7,30 @@ export default class ScrollingTabsComponent {
   }
 
   tabPanelReady(event) {
-    debugger;
     const tabPanel = event.detail.cmp;
     tabPanel.setTabBar({
       layout: {
           pack: 'start',
           overflow: 'scroller',
+          position: 'center',
       }
     });
+
+    tabPanel.setDefaults({
+      ...tabPanel.getDefaults(),
+      tab: { minWidth: "130px" }
+    });
+
+    //
+    // let tabBarcomp = null;
+    // for (let item of tabPanel.items.items) {
+    //   if (item.xtype === 'tabbar') {
+    //     tabBarcomp = item;
+    //   }
+    // }
+    //
+    // for (let tabBar of tabBarcomp.items.items) {
+    //   tabBar.setMinWidth('130px');
+    // }
   }
 }
