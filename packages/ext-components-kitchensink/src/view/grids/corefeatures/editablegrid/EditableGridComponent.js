cbdb5005e5c4c3a5aea9257d8b42f3a5cc8cb191
Rahulgarg30591 <rgarg@celestialsys.com>
2019-03-29 17:44:39 +0530

    Initial Commit of editable grid.

--- packages/ext-components-kitchensink/src/view/grids/corefeatures/editablegrid/EditableGridComponent.js
+++ packages/ext-components-kitchensink/src/view/grids/corefeatures/editablegrid/EditableGridComponent.js
@@ -1,18 +1,54 @@
-import './EditableGridComponent.css';
+import model from '../CompanyModel';
+
+Ext.require([
+  'Ext.grid.plugin.Editable',
+  'Ext.grid.plugin.CellEditing',
+  'Ext.data.validator.Presence',
+  'Ext.data.validator.Number',
+  'Ext.data.validator.Date'
+]);
 
 export default class EditableGridComponent {
+  constructor () {}
 
-  constructor () {
-    console.log('in EditableGridComponent constructor');
+  onReady(event) {
+    debugger;
+    const store = Ext.create('Ext.data.Store', {
+      model,
+      autoLoad: true,
+      pageSize: 0,
+      proxy: {
+        type: 'ajax',
+        url: 'resources/data/CompanyData.json',
+      }
+    });
+
+    const editableGrid = event.detail.cmp;
+    editableGrid.setStore(store);
   }
-
-  readyButton1(event) {
-    var cmp = event.detail.cmp;
-    this.button1Cmp = event.detail.cmp;
-  }
-
-  tapButton1(event) {
-    this.button1Cmp.setText(new Date())
-  }
-
 }
+
+
+
+//
+//
+// signTpl = (field, format, data) => {
+//   const value = data[field];
+//   return (
+//     <span style={{ color: value > 0 ? 'green' : value < 0 ? 'red' : ''}}>
+//       {Ext.util.Format.number(value, format)}
+//     </span>
+//   )
+// }
+//
+// createSignTpl = format => new Template(value => (
+//       <span style={{ color: value > 0 ? 'green' : value < 0 ? 'red' : ''}}>
+//           {Ext.util.Format.number(value, format)}
+//       </span>
+//   ));
+//
+//   renderSign = (format, value) => (
+//       <span style={{ color: value > 0 ? 'green' : value < 0 ? 'red' : ''}}>
+//           {Ext.util.Format.number(value, format)}
+//       </span>
+//   )
