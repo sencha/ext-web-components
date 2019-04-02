6c72d3abb39fcd9080f65dd7d3d6275e0896254c
Shivani Sharma <sharma.14shivani@gmail.com>
2019-04-02 18:42:14 +0530

    completed compactLayout, configuratorPlugin, dataChanges, drillDownPlugin pivot grid components

--- packages/ext-components-kitchensink/src/view/pivotgrids/compactlayout/CompactLayoutComponent.js
+++ packages/ext-components-kitchensink/src/view/pivotgrids/compactlayout/CompactLayoutComponent.js
@@ -1,18 +1,63 @@
-import './CompactLayoutComponent.css';
+import './CompactLayoutComponent.html';
+import { generateData } from '../generateSaleData';
+import SaleModel from '../SaleModel';
 
 export default class CompactLayoutComponent {
-
   constructor () {
-    console.log('in CompactLayoutComponent constructor');
+    this.store = Ext.create('Ext.data.Store', {
+      model: SaleModel,
+      data: generateData()
+    });
+    this.monthLabelRenderer = function(value) {return Ext.Date.monthNames[value];} 
+    this.pivotgridMatrix = {
+      type: 'local',
+      textRowLabels: 'Custom header',
+      compactViewColumnWidth: 210,
+      viewLayoutType: 'compact',
+      store: this.store,
+      aggregate: [{
+          dataIndex: 'value',
+          header: 'Total',
+          aggregator: 'sum',
+          width: 120
+      }],
+      leftAxis: [{
+          dataIndex: 'person',
+          header: 'Person'
+      }, {
+          dataIndex: 'company',
+          header: 'Company',
+          sortable: false
+      }, {
+          dataIndex: 'country',
+          header: 'Country'
+      }],
+      topAxis: [{
+          dataIndex: 'year',
+          header: 'Year'
+      }, {
+          dataIndex: 'month',
+          header: 'Month',
+          labelRenderer: this.monthLabelRenderer
+      }]
+     }
   }
 
-  readyButton1(event) {
-    var cmp = event.detail.cmp;
-    this.button1Cmp = event.detail.cmp;
+  onPivotGridReady(event) {
+    this.pivotgrid = event.detail.cmp;
+    this.pivotgrid.setMatrix(this.pivotgridMatrix);
   }
 
-  tapButton1(event) {
-    this.button1Cmp.setText(new Date())
+  onReadyButton1(event){
+    this.Button1 = event.detail.cmp;
+    this.Button1.setHandler(this.expandAll.bind(this));
+  }
+ 
+  onReadyButton2(event){
+    this.Button2 = event.detail.cmp;
+    this.Button2.setHandler(this.collapseAll.bind(this));
   }
 
+  expandAll() { this.pivotgrid.expandAll() }
+  collapseAll() { this.pivotgrid.collapseAll() }
 }
