15fc2436dbae14e1d95bb257a0e1601e88afd58b
Palak Rathi <palakrathi@CelSys053-Palak-Rathi.local>
2019-04-03 17:26:47 +0530

    EWC-4-Added columns

--- packages/ext-components-kitchensink/src/view/charts/column/columnwithrenderer/ColumnWithRendererComponent.js
+++ packages/ext-components-kitchensink/src/view/charts/column/columnwithrenderer/ColumnWithRendererComponent.js
@@ -1,18 +1,65 @@
-import './ColumnWithRendererComponent.css';
+import './ColumnWithRendererComponent.html';
+import { createData } from './ColumnWithRendererComponentData.js';
+import { seriesG1Renderer, seriesG2Renderer } from './ColumnWithRendererComponentRenderer.js';
 
 export default class ColumnWithRendererComponent {
 
   constructor () {
     console.log('in ColumnWithRendererComponent constructor');
+    this.store = Ext.create('Ext.data.Store', {
+      fields: ['id', 'g1', 'g2', 'g3', 'g4', 'g5', 'g6', 'name']
+    });
+    this.numRecords = 10; 
   }
 
-  readyButton1(event) {
-    var cmp = event.detail.cmp;
-    this.button1Cmp = event.detail.cmp;
+  onRefreshButtonReady(event) {
+    this.refreshButtonCmp = event.detail.cmp;
+    this.refreshButtonCmp.on('tap', this.onRefreshClick.bind(this));
   }
 
-  tapButton1(event) {
-    this.button1Cmp.setText(new Date())
+  onRefreshClick(event) {
+    this.store.loadData(createData(this.numRecords));
+    this.cartesianCmp.setStore(this.store);
+
   }
 
+  cartesianready(event) {
+    this.cartesianCmp = event.detail.cmp;
+    this.cartesianCmp.setStore(this.store);
+    this.cartesianCmp.setAxes([{
+      type: 'numeric',
+      position: 'left',
+      fields: ['g1', 'g2'],
+      minimum: 0
+    }, {
+      type: 'category',
+      position: 'bottom',
+      fields: 'name'
+    }]);
+    this.cartesianCmp.setSeries([{
+      type: 'bar',
+      xField: 'name',
+      yField: 'g1',
+      renderer: seriesG1Renderer.bind(this),
+      style: {
+        lineWidth: 2,
+        maxBarWidth: 30,
+        stroke: 'dodgerblue',
+        fill: 'palegreen',
+        opacity: 0.6
+      }
+    }, {
+      type: 'bar',
+      xField: 'name',
+      yField: ['g2'],
+      renderer: seriesG2Renderer.bind(this),
+      style: {
+        lineWidth: 2,
+        maxBarWidth: 12,
+        stroke: 'tomato',
+        fill: 'lightyellow',
+        radius: 20
+      }
+    }]);
+  }
 }
--- packages/ext-components-kitchensink/src/view/charts/column/stacked/StackedComponent.js
+++ packages/ext-components-kitchensink/src/view/charts/column/stacked/StackedComponent.js
@@ -1,18 +1,110 @@
-import './StackedComponent.css';
+import './StackedComponent.html';
+import createData from './StackedComponentData.js';
 
 export default class StackedComponent {
 
   constructor () {
     console.log('in StackedComponent constructor');
+    this.store = Ext.create('Ext.data.Store', {
+      fields: ['id', 'g0', 'g1', 'g2', 'g3', 'g4', 'g5', 'g6', 'name']
+    });
+    this.theme = "default";
+    this.menuCmpArray = [];
+    this.stacked = 0;
+    this.zoom = false;
+    this.numRecords = 7;
+    this.store.loadData(createData(this.numRecords));
+
   }
 
-  readyButton1(event) {
-    var cmp = event.detail.cmp;
-    this.button1Cmp = event.detail.cmp;
+
+  onMenuItemReady(event) {
+    this.menuCmpArray.push(event.detail.cmp);
+    event.detail.cmp.on('click', this.onThemeChange.bind(this));
   }
 
-  tapButton1(event) {
-    this.button1Cmp.setText(new Date())
+  onRefreshButtonReady(event) {
+    this.refreshButtonCmp = event.detail.cmp;
+    this.refreshButtonCmp.on('tap', this.onRefreshClick.bind(this));
   }
+
+  onRefreshClick(event) {
+    this.store.loadData(createData());
+    this.cartesianCmp.setStore(this.store);
+
+  }
+
+  toggleZoomOnPan(zoomOnPan) {
+    //Added cmp to access component attributes in ext-react16 [revisit]
+    this.chart.getInteraction('panzoom').setZoomOnPan(zoomOnPan);
+    this.zoom = zoomOnPan;
+  };
+
+  onStackedToggle(event) {
+    if (event.value == 0) {
+      this.stacked = 1;
+    } else {
+      this.stacked = 0;
+    }
+
+    this.chart.getSeries()[0].setStacked(this.stacked);
+    this.chart.redraw();
+  };
+
+  onThemeChange(event) {
+    this.theme = event.config.text.toLowerCase();
+    this.menuCmpArray.forEach((cmp, index) => {
+      if (index == parseInt(event.config.itemId)) {
+        cmp.setIconCls('x-font-icon md-icon-done');
+        return;
+      }
+      cmp.setIconCls('');
+    });
+    this.cartesianCmp.setTheme(event.config.text.toLowerCase());
+  }
+
+  containerready(event) {
+    this.cmp = event.detail.cmp;
+  }
+
+  cartesianready(event) {
+    this.chart = event.ext;
+    this.cartesianCmp = event.detail.cmp;
+    this.cartesianCmp.setStore(this.store);
+    this.cartesianCmp.setTheme(this.theme);
+    this.cartesianCmp.setAxes([{
+      type: 'numeric',
+      position: 'left',
+      fields: ['g1', 'g2', 'g3', 'g4', 'g5', 'g6']
+    }, {
+      type: 'category',
+      position: 'bottom',
+      fields: 'name'
+    }]);
+    this.cartesianCmp.setSeries([{
+      type: 'bar',
+      xField: 'name',
+      yField: ['g1', 'g2', 'g3', 'g4', 'g5', 'g6'],
+      title: ['Apples', 'Oranges', 'Bananas', 'Plums', 'Mangos', 'Pears'],
+      stacked: true,
+      style: {
+        minGapWidth: 15
+      }
+    }]);
+    this.cartesianCmp.setInteractions([{
+      type: 'panzoom',
+      axes: {
+        left: {
+          allowPan: false,
+          allowZoom: false
+        },
+        bottom: {
+          allowPan: true,
+          allowZoom: true
+        }
+      }
+    }]);
+  }
+
 
 }
