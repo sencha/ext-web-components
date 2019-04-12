9fad27c47644f41203f17b5da14bb84ff84e6615
MedhaSingh32 <singhmedha32@gmail.com>
2019-04-12 17:34:40 +0530

    calendar completed

--- packages/ext-components-conference-app/src/view/calendar/CalendarComponent.js
+++ packages/ext-components-conference-app/src/view/calendar/CalendarComponent.js
@@ -1,45 +1,54 @@
 import "./CalendarComponent.html";
-import '../event/CustomEvent.js';
 
 export default class CalendarComponent {
-  constructor(props) { 
+  constructor(props) {
     localStorage.setItem('favoriteEvents', JSON.stringify(['57f6ddc31ff4d', '57ee3b237afcb']));
-    this.hideEvent = true;
+    this.showEvent = false;
     const favs = localStorage.getItem('favoriteEvents');
     this.favorites = favs ? JSON.parse(favs) : [];
   }
 
-  containerReady(event) {
-    console.log('çontainer is ready')
-  }
-
   calendarReady(event) {
-    console.log('calendar is ready')
     const store = Ext.create('Ext.calendar.store.Calendars', {
-        eventStoreDefaults: {
-            proxy: {
-                type: 'ajax',
-                url: 'resources/schedule.json'
-            },
-            filters: item => this.favorites.indexOf(item.get('id')) >= 0
+      eventStoreDefaults: {
+        proxy: {
+          type: 'ajax',
+          url: 'resources/schedule.json'
         },
-        data: [{
-            id: 1,
-            name: 'myCal'
-        }]
+        filters: item => this.favorites.indexOf(item.get('id')) >= 0
+      },
+      data: [{
+        id: 1,
+        name: 'myCal'
+      }]
     })
     this.calendar = event.detail.cmp;
     this.calendar.setValue(new Date(2016, 10, 7));
     this.calendar.setStore(store);
   }
 
-  onTap(event){
-      console.log(event.detail.context.event.data)
-      let eventDetail = event.detail.context.event.data;
-      eventDetail = JSON.stringify(eventDetail);
-      console.log(eventDetail,'eventDetail');
-      this.showEvent = !this.showEvent;
-      document.getElementById('customEvent').setAttribute('hide', this.showEvent);
-      document.getElementById('customEvent').setAttribute('event', eventDetail);
+  onTap(event) {
+    let data = event.detail.context.event.data;
+    const day = data && data.date && data.date.match(/(Monday|Tuesday|Wednesday)/)[1];
+    this.sidePanel.setHidden(false);
+    this.sidePanel.setFlex(1);
+    let horizontalRule = '';
+    if (data.description) {
+      horizontalRule = '<hr/>';
+    }
+
+    this.sidePanel.setHtml(`
+      <div>
+                    <div class="app-event-name">${data.title}</div>
+                    <div class="app-event-time">${day} ${data.start_time} - ${data.end_time}</div>
+                    <div class="app-event-location">${data.location.name}</div>
+                    ${horizontalRule}
+                    <div class="app-event-abstract">${data.description}</div>
+                </div>`
+    )
+  }
+
+  sidePanelReady(event) {
+    this.sidePanel = event.detail.cmp;
   }
 }
