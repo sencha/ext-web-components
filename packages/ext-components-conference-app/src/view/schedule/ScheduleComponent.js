b906e7da83e91d57b3451e05e21f358169698306
Shivani Sharma <sharma.14shivani@gmail.com>
2019-04-12 12:41:04 +0530

    working on schedule component

--- packages/ext-components-conference-app/src/view/schedule/ScheduleComponent.js
+++ packages/ext-components-conference-app/src/view/schedule/ScheduleComponent.js
@@ -27,14 +27,6 @@ export default class ScheduleComponent {
     //   getQuery: this.getQuery, 
     //   showTime
     // });
-    this.itemTpl = `<div className="app-list-content">
-                   <div className="app-list-text">
-                      <div className="app-list-item-title" dangerouslySetInnerHTML={title}/>
-                      <div className="app-list-item-details">{detail}</div> 
-                      <div className="app-list-item-details">{categoryName} - {location.name}</div>
-                      {start_time}
-                   </div>
-               </div>`
   }
 
   // createTpl({ getQuery, showTime=true }) {
@@ -61,6 +53,54 @@ export default class ScheduleComponent {
   //   }
   // }
 
+  // onItemTap = (list, index, target, record) => {
+  //   if (record) {
+  //       self.location.hash = `/schedule/${record.id}`;
+  //   }
+
+  //   if (this.props.onSelect) {
+  //       this.props.onSelect(record);
+  //   }
+  // }
+
+  containerready(event) {
+    alert('m here')
+    this.containerCmp = event.detail.cmp;
+  }
+
+  onItemTap(location, eopts) {
+    console.log(eopts);
+    this.record = eopts.record.data;
+    this.speakerId = eopts.record.id;
+    if (this.sideContainer) {
+      this.sideContainer.remove(this.speakerChild);
+      this.containerCmp.remove(this.sideContainer);
+    }
+
+    this.sideContainer = Ext.create({
+      xtype: 'panel',
+      flex: '1',
+      layout: 'vbox',
+      padding: '20',
+    });
+    this.speakerChild = Ext.create({
+      xtype: 'container',
+      html: `
+                <div class="app-speaker-ct">
+                            <img class="app-speaker-image" src=${this.record.avatar_url}></img>
+                            <div class="app-speaker-text">
+                                <div class="app-speaker-name">${this.record.name}</div>
+                                <div class="app-speaker-title">${this.record.title}</div>
+                                <div class="app-speaker-company">${this.record.company}</div>
+                                <div class="app-speaker-bio">${this.record.bio}</div>
+                            </div>
+                        </div>
+                    `,
+    });
+    this.sideContainer.add(this.speakerChild);
+    this.containerCmp.add(this.sideContainer);
+  }
+
   firstListReady(event) {
     const store = Ext.create(
       'Ext.data.ChainedStore',
@@ -69,11 +109,21 @@ export default class ScheduleComponent {
         filters: [{ property: 'date', value: 'Monday, November 7' }]
       }
     );
+    const itemTpl = `<div class="app-list-content">
+                   <div class="app-list-text">
+                      <div class="app-list-item-title">{title}</div>
+                      <div class="app-list-item-details">{detail}</div> 
+                      <div class="app-list-item-details">{categoryName} - {location.name}</div>
+                      {start_time}
+                   </div>
+                   <div
+                      data-favorite="on"
+                      class="x-item-no-tap x-font-icon md-icon-star app-list-tool app-favorite"
+                    />
+               </div>`
     this.list1 = event.detail.cmp;
+    this.list1.setItemTpl(itemTpl);
     this.list1.setStore(store);
-    console.log(this.itemTpl);
-
-    this.list1.setItemTpl(this.itemTpl)
   }
 
   secondListReady(event) {
@@ -84,7 +134,16 @@ export default class ScheduleComponent {
         filters: [{ property: 'date', value: 'Tuesday, November 8' }]
       }
     );
+    const itemTpl = `<div class="app-list-content">
+                   <div class="app-list-text">
+                      <div class="app-list-item-title">{title}</div>
+                      <div class="app-list-item-details">{detail}</div> 
+                      <div class="app-list-item-details">{categoryName} - {location.name}</div>
+                      {start_time}
+                   </div>
+               </div>`
     this.list2 = event.detail.cmp;
+    this.list2.setItemTpl(itemTpl);
     this.list2.setStore(store);
   }
 
@@ -96,7 +155,20 @@ export default class ScheduleComponent {
         filters: [{ property: 'date', value: 'Wednesday, November 9' }]
       }
     );
+    const itemTpl = `<div class="app-list-content">
+                   <div class="app-list-text">
+                      <div class="app-list-item-title">{title}</div>
+                      <div class="app-list-item-details">{detail}</div> 
+                      <div class="app-list-item-details">{categoryName} - {location.name}</div>
+                      {start_time}
+                   </div>
+                   <div 
+                        
+                        class="x-item-no-tap x-font-icon md-icon-star app-list-tool app-favorite"
+                    />
+               </div>`
     this.list3 = event.detail.cmp;
+    this.list3.setItemTpl(itemTpl);
     this.list3.setStore(store);
   }
 
