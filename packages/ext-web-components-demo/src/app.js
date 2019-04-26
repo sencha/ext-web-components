dba9cf1074dd0f816273223835838a2ebf5a21c8
mgusmano <mgusmano@yahoo.com>
2019-04-26 15:37:54 -0400

    fix for handler

--- packages/ext-web-components-demo/src/app.js
+++ packages/ext-web-components-demo/src/app.js
@@ -1,32 +1,37 @@
-//import '@sencha/ext-components'
+//import '@sencha/ext-web-components'
 
-import '@sencha/ext-components/lib/ext-carousel.component';
+import '@sencha/ext-web-components/lib/ext-carousel.component';
 
-import '@sencha/ext-components/lib/ext-container.component';
+import '@sencha/ext-web-components/lib/ext-container.component';
 
 
 
-import '@sencha/ext-components/lib/ext-panel.component';
+import '@sencha/ext-web-components/lib/ext-panel.component';
 
-import '@sencha/ext-components/lib/ext-button.component';
-import '@sencha/ext-components/lib/ext-dataview.component';
-import '@sencha/ext-components/lib/ext-searchfield.component';
-import '@sencha/ext-components/lib/ext-treelist.component';
-import '@sencha/ext-components/lib/ext-tabpanel.component';
-import '@sencha/ext-components/lib/ext-titlebar.component';
-import '@sencha/ext-components/lib/ext-toolbar.component';
-import '@sencha/ext-components/lib/ext-grid.component';
-import '@sencha/ext-components/lib/ext-column.component';
+import '@sencha/ext-web-components/lib/ext-button.component';
+import '@sencha/ext-web-components/lib/ext-dataview.component';
+import '@sencha/ext-web-components/lib/ext-searchfield.component';
+import '@sencha/ext-web-components/lib/ext-treelist.component';
+import '@sencha/ext-web-components/lib/ext-tabpanel.component';
+import '@sencha/ext-web-components/lib/ext-titlebar.component';
+import '@sencha/ext-web-components/lib/ext-toolbar.component';
+import '@sencha/ext-web-components/lib/ext-grid.component';
+import '@sencha/ext-web-components/lib/ext-column.component';
 
 import CarouselComponent from './view/carousel/CarouselComponent.js';
+import ButtonComponent from './view/button/ButtonComponent.js';
 
 (function () {
 
   init();
 
   function init() {
-    window.carousel = new CarouselComponent()
-    document.body.innerHTML = window._code['carousel']['CarouselComponent.html']
+    //window.carousel = new CarouselComponent()
+//    document.body.innerHTML = window._code['carousel']['CarouselComponent.html']
+
+    window.button = new ButtonComponent()
+    document.body.innerHTML = window._code['button']['ButtonComponent.html']
+
   }
 
   }());
