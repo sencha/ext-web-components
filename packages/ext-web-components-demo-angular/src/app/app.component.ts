cbfe56bf7c2ce7564c4044f0e89d60d03512609c
mgusmano <mgusmano@yahoo.com>
2019-05-02 08:32:28 -0400

    examples

--- packages/ext-web-components-demo-angular/src/app/app.component.ts
+++ packages/ext-web-components-demo-angular/src/app/app.component.ts
@@ -6,58 +6,38 @@ import { SampleData } from './SampleData';
   selector: 'app-root',
   styles: [``],
   template: `
-  <ext-container padding="10" layout="fit" [fitToParent]="true">
-    <ext-grid [title]="this.gridTitle" shadow="true" (ready)="this.readyGrid($event)">
+  <ext-panel title="Sencha ExtWebComponents 7.0 in Angular" layout="vbox" fitToParent="true" padding="10">
+    <div style="text-align:center;margin-top:20px;margin-bottom:25px;font-size:24px;">
+      <img width="250" alt="Angular" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg==">
+      <div>Angular</div>
+    </div>
+    <ext-grid flex="1" fitToParent="true" title="Employees" shadow="true" (ready)="this.readyGrid($event)">
       <ext-toolbar docked="top">
-        <ext-searchfield ui="faded" placeholder="Search..." (change)="this.onSearch($event)" [responsiveConfig]="searchresponsiveConfig"></ext-searchfield>
+        <ext-searchfield ui="faded" placeholder="Search..." (change)="this.onSearch($event)"></ext-searchfield>
       </ext-toolbar>
-      <ext-column text="ID" dataIndex="id" width="50px" resizable="true"></ext-column>
-      <ext-column text="Name" dataIndex="name" flex="2" resizable="true"></ext-column>
-      <ext-column text="Email" dataIndex="email" flex="3" resizable="true" [responsiveConfig]="emailresponsiveConfig"></ext-column>
-      <ext-column text="Phone" dataIndex="phone" flex="2" resizable="true"></ext-column>
+      <ext-column text="ID"    dataIndex="id"    width="50"></ext-column>
+      <ext-column text="Name"  dataIndex="name"  flex="2">  </ext-column>
+      <ext-column text="Email" dataIndex="email" flex="3">  </ext-column>
+      <ext-column text="Phone" dataIndex="phone" flex="2">  </ext-column>
     </ext-grid>
-  </ext-container>
+  </ext-panel>
   `
 })
 export class AppComponent {
 
-  isPhone = Ext.platformTags.phone;
-
-  store = Ext.create('Ext.data.Store', {
+  store:any = Ext.create('Ext.data.Store', {
     fields: ['name', 'email', 'phone', 'hoursTaken', 'hoursRemaining'],
     data: new SampleData(50).data
   });
 
-  emailresponsiveConfig = {
-    small: {
-      hidden: true
-    },
-    medium: {
-      hidden: false
-    }
-  };
-
-  searchresponsiveConfig = {
-    small: {
-      flex: 1
-    },
-    medium: {
-      flex: undefined
-    }
-  };
-
   gridCmp: any;
-  gridTitle: string = 'Sencha ExtWebComponents 7.0 in Angular';
 
-  readyGrid(event) {
+  readyGrid = (event) => {
     this.gridCmp = event.detail.cmp
-    //console.log('readyGrid')
-    //console.log(event)
     this.gridCmp.setStore(this.store)
   }
 
   onSearch = (event) => {
-    //console.dir(event)
     const query = event.detail.newValue.toLowerCase();
     this.store.clearFilter();
     if (query.length) this.store.filterBy(record => {
