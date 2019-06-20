27036a1d34b2289a153ff427730f7cfbbb055fcb
pooja-awasthi <awasthipooja66@gmail.com>
2019-06-20 14:32:59 +0530

    EWC-5-APIDOCS-Example fiddle example of slider components updated

--- api-docs/modern/modern/src/field/Slider.js
+++ api-docs/modern/modern/src/field/Slider.js
@@ -82,52 +82,6 @@
  *      }
  *      ```
  *
- *     @example packages=[reactor]
- *     import React, { Component } from 'react';
- *     import { ExtReact, Container, FormPanel, SliderField } from '@extjs/ext-react';
- *
- *     export default class SliderFieldExample extends Component {
- *
- *         state = {
- *             singleValue: 20,
- *             multipleValue: [10, 70]
- *         };
- *
- *         onSingleChange = (field, value) => {
- *             this.setState({ singleValue: value });
- *         }
- *
- *         onMultipleChange = (field, value) => {
- *             this.setState({ multipleValue: value });
- *         }
- *
- *         render() {
- *             const { singleValue, multipleValue } = this.state;
- *
- *             return (
- *                 <ExtReact>
- *                     <Container layout="center">
- *                         <FormPanel shadow width="300">
- *                             <SliderField
- *                                 onChange={this.onSingleChange}
- *                                 label="Single Thumb"
- *                                 value={singleValue}
- *                             />
- *                             <div style={{marginBottom: '20px'}}>Value: {singleValue}</div>
- *                             <SliderField
- *                                 onChange={this.onMultipleChange}
- *                                 label="Multiple Thumbs"
- *                                 values={multipleValue}
- *                             />
- *                             <div>Values: {multipleValue.join(', ')}</div>
- *                         </FormPanel>
- *                     </Container>
- *                 </ExtReact>
- *             )
- *         }
- *
- *     }
- *
  */
 
 /**
