27f71444ff8b727df8180ccf56820d7af5c71639
Rahulgarg30591 <rgarg@celestialsys.com>
2019-07-18 18:24:18 +0530

    EWC-4-KS-Froala Editor is completed.

--- packages/ext-web-components-kitchensink/src/view/components/froalaeditor/FroalaEditorComponent.js
+++ packages/ext-web-components-kitchensink/src/view/components/froalaeditor/FroalaEditorComponent.js
@@ -1,18 +1,51 @@
 Ext.require([ 'Ext.froala.EditorField' ]);
+import 'froala-editor/css/froala_editor.pkgd.min.css';
 import './FroalaEditorComponent.html';
 
 export default class FroalaEditorComponent {
-    constructor() {}
+    constructor() {
+      this.value='<p>Four score and seven years ago.</p>';
+    }
 
-    onTopLabelReady(event) {
+    parentFormPanelReady(event) {
+      this.parentFormPanelCmp = event.detail.cmp;
+      this.parentFormPanelCmp.setButtons({
+        ok: {
+            text: 'getValues()',
+            handler: this.formPanelOkBtnClick
+          }
+      });
+    }
+
+    formPanelOkBtnClick = () => {
+      Ext.Msg.alert('getValues()', Ext.JSON.encode(this.parentFormPanelCmp.getValues()));
+    }
+
+    onTopLabelReady = (event) => {
       this.formPanelTopLabelCmp = event.detail.cmp;
+      this.formPanelTopLabelCmp.setHtml(this.value);
     }
 
-    onEditorFieldReady(event) {
-      this.editorFieldCmp = event.detail.cmp;
+    onEditorFieldReady = (event) => {
+      if (event.detail.cmp) {
+        this.editorFieldCmp = event.detail.cmp;
+        this.editorFieldCmp.setValue(this.value);
+
+        this.editorFieldCmp.setListeners({
+          'froala.click': (froalaComponent) => { Ext.toast({ message: 'Click!' }); }
+        });
+      }
     }
 
-    onBottomLabelReady(event) {
-      this.bottomLabelCmp = event.detail.cmp;
+    onBottomLabelReady = (event) => {
+      this.formPanelBottomLabelCmp = event.detail.cmp;
+      this.formPanelBottomLabelCmp.setHtml(Ext.htmlEncode(this.value));
+    }
+
+    froalaTextChange = (event) => {
+      this.value = event.detail.the;
+      this.formPanelBottomLabelCmp.setHtml(Ext.htmlEncode(this.value));
+      this.formPanelTopLabelCmp.setHtml(this.value);
+      Ext.toast({ message: 'Change!' });
     }
 }
