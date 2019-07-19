b2c6de1ffffa8a080718a6052c07f5a8772e753d
Rahulgarg30591 <rgarg@celestialsys.com>
2019-07-19 15:53:40 +0530

    Froala is updated.

--- packages/ext-web-components-kitchensink/src/view/components/froalaeditor/FroalaEditorComponent.js
+++ packages/ext-web-components-kitchensink/src/view/components/froalaeditor/FroalaEditorComponent.js
@@ -3,9 +3,7 @@ import 'froala-editor/css/froala_editor.pkgd.min.css';
 import './FroalaEditorComponent.html';
 
 export default class FroalaEditorComponent {
-    constructor() {
-      this.value='<p>Four score and seven years ago.</p>';
-    }
+    constructor() {}
 
     parentFormPanelReady(event) {
       this.parentFormPanelCmp = event.detail.cmp;
@@ -21,15 +19,9 @@ export default class FroalaEditorComponent {
       Ext.Msg.alert('getValues()', Ext.JSON.encode(this.parentFormPanelCmp.getValues()));
     }
 
-    onTopLabelReady = (event) => {
-      this.formPanelTopLabelCmp = event.detail.cmp;
-      this.formPanelTopLabelCmp.setHtml(this.value);
-    }
-
     onEditorFieldReady = (event) => {
       if (event.detail.cmp) {
         this.editorFieldCmp = event.detail.cmp;
-        this.editorFieldCmp.setValue(this.value);
 
         this.editorFieldCmp.setListeners({
           'froala.click': (froalaComponent) => { Ext.toast({ message: 'Click!' }); }
@@ -37,15 +29,7 @@ export default class FroalaEditorComponent {
       }
     }
 
-    onBottomLabelReady = (event) => {
-      this.formPanelBottomLabelCmp = event.detail.cmp;
-      this.formPanelBottomLabelCmp.setHtml(Ext.htmlEncode(this.value));
-    }
-
     froalaTextChange = (event) => {
-      this.value = event.detail.the;
-      this.formPanelBottomLabelCmp.setHtml(Ext.htmlEncode(this.value));
-      this.formPanelTopLabelCmp.setHtml(this.value);
       Ext.toast({ message: 'Change!' });
     }
 }
