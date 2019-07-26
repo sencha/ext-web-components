59889be95c917eaf8def5a15d430daa7d7794bbb
Rahulgarg30591 <rgarg@celestialsys.com>
2019-07-26 19:13:46 +0530

    EWC-5 Api Docs are updated with context.

--- api-docs/modern/modern/src/dataview/SimpleListItem.js
+++ api-docs/modern/modern/src/dataview/SimpleListItem.js
@@ -1,14 +1,11 @@
 /**
+ * @class Ext.dataview.SimpleListItem
+ * @extend Ext.Component
+ * @xtype simplelistitem
  * This component is the default data item used by {@link Ext.dataview.List}.
  *
  * This component supports disclosure icons and generates the slimmest markup possible for
  * a list data item. It doesn't support container functionality like adding or docking
- * items. To enable those features, use {@link Ext.dataview.ListItem} instead:
+ * items. To enable those features, use {@link Ext.dataview.ListItem} instead.
  *
- *      {
- *          xtype: 'list',
- *          itemConfig: {
- *              xtype: 'listitem'
- *          }
- *      }
  */
