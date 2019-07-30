90710e2106eb64e26434af5269a037cbd62bb330
Rahulgarg30591 <rgarg@celestialsys.com>
2019-07-30 16:42:08 +0530

    EWC-23 Angular Boilerplate is created using Angular CLI with docs.

--- packages/ext-web-components-boilerplate-angular-cli/src/app/app.module.ts
+++ packages/ext-web-components-boilerplate-angular-cli/src/app/app.module.ts
@@ -1,20 +1,19 @@
-declare var Ext: any
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { AppRoutingModule }     from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 
-import '@sencha/ext-web-components/lib/ext-container.component';
-import '@sencha/ext-web-components/lib/ext-viewport.component';
-import '@sencha/ext-web-components/lib/ext-dataview.component';
-import '@sencha/ext-web-components/lib/ext-searchfield.component';
-import '@sencha/ext-web-components/lib/ext-grid.component';
-import '@sencha/ext-web-components/lib/ext-button.component';
-import '@sencha/ext-web-components/lib/ext-treelist.component';
-import '@sencha/ext-web-components/lib/ext-panel.component';
-import '@sencha/ext-web-components/lib/ext-titlebar.component';
-import '@sencha/ext-web-components/lib/ext-toolbar.component';
-import '@sencha/ext-web-components/lib/ext-column.component';
-import '@sencha/ext-web-components/lib/ext-widget.component';
+import '@sencha/ext-web-components/dist/ext-container.component';
+import '@sencha/ext-web-components/dist/ext-viewport.component';
+import '@sencha/ext-web-components/dist/ext-dataview.component';
+import '@sencha/ext-web-components/dist/ext-searchfield.component';
+import '@sencha/ext-web-components/dist/ext-grid.component';
+import '@sencha/ext-web-components/dist/ext-button.component';
+import '@sencha/ext-web-components/dist/ext-treelist.component';
+import '@sencha/ext-web-components/dist/ext-panel.component';
+import '@sencha/ext-web-components/dist/ext-titlebar.component';
+import '@sencha/ext-web-components/dist/ext-toolbar.component';
+import '@sencha/ext-web-components/dist/ext-column.component';
+import '@sencha/ext-web-components/dist/ext-widget.component';
 
 import { AppComponent } from './app.component'
 import AboutComponent from './about/about.component'
@@ -34,4 +33,5 @@ import HomeComponent from './home/home.component'
   schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+}
