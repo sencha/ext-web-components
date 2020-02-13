3964ce2f2e1af28eb41ac43b1a80972f4451facb
mgusmano <mgusmano@yahoo.com>
2020-02-13 08:27:07 -0500

    packages

--- packages/rm-node.sh
+++ packages/rm-node.sh
@@ -1,9 +1,9 @@
-rm -r ext-web-components/node_modules
 rm -r ext-web-components-boilerplate/node_modules
 rm -r ext-web-components-boilerplate-angular-cli/node_modules
 rm -r ext-web-components-boilerplate-create-react-app/node_modules
+rm -r ext-web-components-boilerplate-svelte/node_modules
 rm -r ext-web-components-boilerplate-vue-cli/node_modules
-rm -r ext-web-components-compress/node_modules
+rm -r ext-web-components-classic/node_modules
 rm -r ext-web-components-conference-app/node_modules
 rm -r ext-web-components-demo/node_modules
 rm -r ext-web-components-demo-angular/node_modules
@@ -11,5 +11,5 @@ rm -r ext-web-components-demo-react/node_modules
 rm -r ext-web-components-demo-vue/node_modules
 rm -r ext-web-components-gen/node_modules
 rm -r ext-web-components-kitchensink/node_modules
-rm -r ext-web-components-renderercell/node_modules
+rm -r ext-web-components-modern/node_modules
 rm -r ext-web-components-tests/node_modules
