6fdadd27f1ee8a56bfc36e750d71ceeefc869ced
mgusmano <mgusmano@yahoo.com>
2020-02-21 14:18:24 -0500

    runtime

--- packages/ext-web-components-modern/dist/runtime/ElementParser.js
+++ packages/ext-web-components-modern/dist/runtime/ElementParser.js
@@ -3,11 +3,10 @@ import _wrapNativeSuper from "@babel/runtime/helpers/wrapNativeSuper";
 
 var ElementParser = function () {
   var toolkit = 'modern';
-  var theme = 'material';
-  var origCwd = process.cwd();
-  var target = 'node_modules';
-  var v;
-  var n;
+  var theme = 'material'; //var origCwd = process.cwd();
+  //var target = 'node_modules'
+  //var v
+  //var n
 
   if (window['Ext'] == undefined) {
     var linkIt = function linkIt(num) {
@@ -42,19 +41,17 @@ var ElementParser = function () {
       console.error('error');
       document.body.innerHTML = "<div>" + "<h1>An error has occurred</h1>" + "The ExtWebComponents runtime cannot be found<p>" + "Possible reasons:<br>" + "<ul>" + "<li>node_modules folder is not found or corrupted (rerun npm install)" + "</ul>" + "</div>";
       window.stop();
-    };
+    }; //v = process.cwd()
+    //console.log(v)
+    //n = v.includes(target)
+    //console.log(n)
+    //while (n == true) {
+    //  process.chdir('../');
+    //  v = process.cwd()
+    //  console.log(v)
+    //  n = v.includes(target)
+    //}
 
-    v = process.cwd();
-    console.log(v);
-    n = v.includes(target);
-    console.log(n);
-
-    while (n == true) {
-      process.chdir('../');
-      v = process.cwd();
-      console.log(v);
-      n = v.includes(target);
-    }
 
     console.warn('[Deprecation] error below is expected');
 
@@ -99,9 +96,8 @@ var ElementParser = function () {
       default:
         console.error('ERROR');
         break;
-    }
+    } //process.chdir(origCwd);
 
-    process.chdir(origCwd);
 
     if (Ext.isModern == true) {
       var ElementCell = Ext.define('Ext.ElementCell', {
